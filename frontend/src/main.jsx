fea64f95f5003d6c52e0670c68e21ebd6863d8a8
SpeedOfLight03 <arnab.ghosh.it2021@kgec.ac.in>
2024-08-07 15:16:02 +0530

    initial setup for chatio

---
+++ frontend/src/main.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
+import Home from './components/Home/Home.jsx';
+import Login from './components/Login/Login.jsx';
+import Chat from './components/Chat/Chat.jsx';
+import Register from './components/Register/Register.jsx';
+
+
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path='/' element={<App />}>
+            <Route path='' element={<Home />}/>
+            <Route path='register' element={<Register />}/>
+            <Route path='login' element={<Login />}/>
+            <Route path='chats' element={<Chat />}/>
+        </Route>
+    )
+)
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
